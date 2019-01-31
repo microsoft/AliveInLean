a8822290b9677909bb62c159fde133c3f31865bc
Juneyoung Lee <aqjune@gmail.com>
2019-02-01 08:35:14 +0900

    Add never_poison

---
+++ src/spec/customlemmas.lean
@@ -0,0 +1,112 @@
+import ..lang
+import ..irsem
+import ..irsem_exec
+import .lemmas_basic
+import .lemmas
+
+open irsem_exec
+
+-- a single small-step function
+@[simp]
+def step := irsem.step_exe irsem_exec
+
+-- a new udiv instruction
+@[simp]
+def udiv (isz:nat) (name:string) (op1 op2:operand): instruction
+  := instruction.binop (ty.int isz) (reg.r name) (bopcode.udiv) [] op1 op2
+
+-- ub check
+@[simp]
+def has_ub (st:irsem.irstate irsem_exec):Prop :=
+  irsem.irstate.getub irsem_exec st = ff
+
+-- get value
+@[simp]
+def get_value (st:irsem.irstate irsem_exec) (op:operand) (opty:ty) :=
+  irsem.get_value irsem_exec st op opty
+
+-- is poison?
+@[simp]
+def is_poison: irsem.valty irsem_exec → Prop
+| (irsem.valty.ival sz o b) :=
+  b = (@bool_like.ff irsem_exec.poisonty irsem_exec.pbl)
+
+set_option eqn_compiler.zeta true
+set_option pp.proofs true
+
+lemma never_poison:
+  ∀ isz name op1 op2 st st' val
+    (HSTEP:step st (udiv isz name op1 op2) = some st')
+    (HNOUB:¬ has_ub st')
+    (HVAL:some val = get_value st op2 (ty.int isz)),
+  ¬ is_poison val
+:= begin
+  intros,
+  cases st with ub regs, cases st' with ub' regs',
+  cases ub'; simp at HNOUB; unfold irsem.irstate.getub at HNOUB,
+    injection HNOUB, clear HNOUB,
+  cases op2, simp at *,
+  {
+    unfold irsem.step_exe at HSTEP,
+    cases (irsem.get_value irsem_exec (ub, regs) op1 (ty.int isz)) with val1;
+      unfold has_bind.bind at *; unfold option.bind at *,
+    injection HSTEP,
+    rw ← HVAL at HSTEP, unfold option.bind at HSTEP,
+    unfold irsem.step_bop at HSTEP,
+    cases val1 with isz1 ii1 ip1,
+    cases val with isz2 ii2 ip2,
+    have HISZDEC:decidable (isz1 = isz2), apply_instance,
+    cases HISZDEC;
+      unfold irsem.step_bop._match_1 at HSTEP;
+      unfold irsem.bop at HSTEP;
+      unfold irsem.step_bop._match_2 at HSTEP;
+      unfold irsem.bop_val at HSTEP,
+    { rw dif_neg at HSTEP, injection HSTEP
+    },
+    {
+      rw (dif_pos HISZDEC) at HSTEP,
+      injection HSTEP with HSTEP', clear HSTEP,
+      generalize HH:
+  (irsem.bop_ub irsem_exec isz1 bopcode.udiv ii1 ip1 (cast (by rw HISZDEC) ii2) ip2)
+    = eub,
+      rw HH at HSTEP',
+      unfold irsem.irstate.updateub at HSTEP',
+      unfold irsem.irstate.updatereg at HSTEP',
+      simp at HSTEP', injection HSTEP' with HUB HREG,
+      cases eub, cases ub; injection HUB,
+      -- now let's unfold irsem.bop_ub.
+      unfold irsem.bop_ub at HH,
+      have HDIV: bop_isdiv bopcode.udiv = tt, by simp,
+      rw (if_pos HDIV) at HH,
+      simp at *,
+      cases ip2,
+      { -- if dividend was poison..
+        unfold_coes at HH, unfold has_and.and at HH,
+        unfold bool_like.and at HH, simp at HH, exfalso, exact HH
+      },
+      {
+        simp
+      }
+    }
+  },
+  {
+    simp at HVAL, unfold irsem.get_value at HVAL,
+    cases op2,
+    dunfold irsem.get_value._match_1 at HVAL,
+    dunfold irsem.get_value._match_2 at HVAL,
+    have HSZDEC1: decidable (0 < isz), apply_instance,
+    cases HSZDEC1,
+    { rw (dif_neg HSZDEC1) at HVAL, injection HVAL },
+    { rw (dif_pos HSZDEC1) at HVAL,
+      simp at HVAL,
+      have HVAL2 := ite_or _ HVAL,
+      cases HVAL2,
+      { injection HVAL2, rw h_1, simp },
+      { have HVAL3 := ite_or _ HVAL2,
+        cases HVAL3,
+        { injection HVAL3, rw h_1, simp },
+        { injection HVAL3 }
+      }
+    }
+  }
+end
